ad78f958a22b24e2255090581020e3cbcede86bc
Dzama <dzamarprosekali@gmail.com>
2025-06-10 21:31:43 +0700

    Test for errors

---
+++ src/main/java/com/coraa/CovenantDataGenerator.java
@@ -0,0 +1,11 @@
+package com.coraa;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CovenantDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
